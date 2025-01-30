ea3888a2c5f78feb0d441ed4f28380747b0ae14b
AshxJay <ashutoshjairam.s2023cse@sece.ac.in>
2025-01-30 14:06:19 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Notepad from './notepad';
+  import Notepad from './notepad';
 import './index.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Real Time Notepad</h1>
+      <h1>Notepad</h1>
       <Notepad />
     </div>
   );
